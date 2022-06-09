5859c753139ff05fcc56fd718058c73f9a2846a4
cocosmos <mipam.guillot@crea-inseec.com>
2022-06-09 09:46:36 +0200

    auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,39 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+
+import AuthReducer from "./AuthReducer";
+
+interface ContextType {
+  dispatch: React.Dispatch<{ type: any; payload: any }>;
+}
+
+const INITIAL_STATE = {
+  currentUser: JSON.parse(localStorage.getItem("user") || "{}"),
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthContext = createContext<ContextType | any>(INITIAL_STATE);
+
+export const AuthContextProvider = ({ children }: any) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
+
+  const value = {
+    alert,
+    currentUser: state.currentUser,
+    dispatch,
+  };
+
+  return <AuthContext.Provider {...{ value }}>{children}</AuthContext.Provider>;
+};
